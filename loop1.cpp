cac1340d1c9881db0923d3f61045386b3c474a78
Puvarasan66 <1171103933@student.mmu.edu.my>
2021-02-05 12:11:16 +0800

    done

--- loop1.cpp
+++ loop1.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+
+    int n =10;
+    for(int i=0; i<=n; i++)
+    {
+        for(int j=1; j<=i; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+
+
+
+    return 0;
+
+}
